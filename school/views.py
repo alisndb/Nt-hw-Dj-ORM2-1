1b1fc3025c01aa5a7398e91d0218a50cdeb6a0fd
alisndb <omarovali6@mail.ru>
2023-01-15 13:04:22 +0300

    Initial commit

---
+++ school/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    ordering = 'group'
+
+    object_list = Student.objects.all().order_by(ordering)
+
+    context = {
+        'object_list': object_list
+    }
+
+    return render(request, template, context)
